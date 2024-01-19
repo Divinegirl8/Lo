39a97405a0a8307f4ed65a42d62597f9d482ca58
James DivineMercy <iamoluchimercy@gmail.com>
2024-01-19 13:58:27 +0100

    changes made

--- src/africa/xLogistics/dtos/requests/RegisterRequest.java
+++ src/africa/xLogistics/dtos/requests/RegisterRequest.java
@@ -1,14 +1,19 @@
 package africa.xLogistics.dtos.requests;
 
 import africa.xLogistics.data.models.Address;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Data
 public class RegisterRequest {
-
+    @NotBlank(message = "field must not be blank")
     private String username;
+    @NotBlank(message = "field must not be blank")
     private String password;
+    @NotBlank(message = "field must not be blank")
     private String emailAddress;
+    @NotBlank(message = "field must not be blank")
     private String phoneNumber;
+    @NotBlank(message = "field must not be blank")
     private Address address;
 }
